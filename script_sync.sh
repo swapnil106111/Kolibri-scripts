9a7454b0c45eddba2001aca64c3812786e12a3d4
swap106111 <swap106111@gmail.com>
2018-03-07 15:09:57 +0530

    sync script for Kolibri 0.4.9

--- script_sync.sh
+++ script_sync.sh
@@ -1,9 +1,23 @@
 #! /bin/sh
-if ping -q -c 1 -W 1 google.com >/dev/null; then
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
   notify-send "You are connected to Internet, Syncing begins.Stay connected."
-  sudo /home/kolibri/kolibri-v0.4.0-beta10.pex manage syncdata -- nalanda
+  output="$(echo 's' | sudo -S /home/kolibri/kolibri-v0.4.9.pex manage syncdata -- nalanda)"
   sleep 10
-  zenity --info --text="Sync Complete! Thank you."
+  
+  if echo "$output" | grep "200 OK"; then
+      zenity --info --text="Sync Complete! Thank you."
+  elif echo "$output" | grep "500"; then
+      zenity --error --text="Internal server error"
+  elif echo "$output" | grep "502"; then
+      zenity --error --text="Bad Gateway Error. Try to connect to the another internet connection and sync"
+  else 
+      zenity --error --text="Not able to sync. Please contact your support team."
+  fi
 else
-   zenity --error --text="Please connect to internet and try again!"
+  zenity --error --text="Please connect to internet and try again!"
 fi
+
+
+
